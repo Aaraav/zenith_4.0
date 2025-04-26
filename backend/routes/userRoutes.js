feb003e02c9a5f4669084c6e03150754e9f62829
Aaraav <76102028+Aaraav@users.noreply.github.com>
2025-04-26 09:46:27 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { saveUser, updateUsername, getUser } = require('../controllers/user');
-// const { submitRating } = require('../controllers/room'); // Import the new controller
 
 const router = express.Router();
 
@@ -9,7 +8,6 @@ router.post('/save-user', saveUser);
 router.put('/update-username', updateUsername);
 router.get('/getUser/:clerkId', getUser);
 
-// 🆕 New route to submit rating
-// router.post('/submit', submitRating);  // Add this line
+
 
 module.exports = router;
